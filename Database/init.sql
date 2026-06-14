e6ca301bd50bdb2095b2a67085276919402f67d4
Martin Mujica <msmujica@hotmail.com>
2026-06-14 14:14:55 -0300

    Se agrega toda la inicializacion del proyecto, osea la parte dockerizada del backend y la base de datos. Como tambien el patron de arquitectura.

---
+++ Database/init.sql
@@ -0,0 +1,8 @@
+CREATE TABLE IF NOT EXISTS usuarios (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    nombre VARCHAR(100) NOT NULL,
+    email VARCHAR(150) NOT NULL UNIQUE
+);
+
+INSERT INTO usuarios (nombre, email)
+VALUES ('Martin', 'martin@test.com');

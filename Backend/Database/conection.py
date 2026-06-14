b3d1c305eef34c19c1e3faf9179c7463391d1abf
Martin Mujica <msmujica@hotmail.com>
2026-06-14 14:40:57 -0300

    Se agrega prueba de db

---
+++ Backend/Database/conection.py
@@ -0,0 +1,29 @@
+import os
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+
+DB_HOST = os.getenv("DB_HOST", "mysql")
+DB_PORT = os.getenv("DB_PORT", "3306")
+DB_NAME = os.getenv("DB_NAME", "ut5_tfu_db")
+DB_USER = os.getenv("DB_USER", "root")
+DB_PASSWORD = os.getenv("DB_PASSWORD", "root")
+
+DATABASE_URL = (
+    f"mysql+pymysql://{DB_USER}:{DB_PASSWORD}@{DB_HOST}:{DB_PORT}/{DB_NAME}"
+)
+
+engine = create_engine(DATABASE_URL)
+
+SessionLocal = sessionmaker(
+    autocommit=False,
+    autoflush=False,
+    bind=engine
+)
+
+
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
--- Backend/main.py
+++ Backend/main.py
@@ -5,3 +5,13 @@ app = FastAPI()
 @app.get("/Health")
 def home():
     return {"mensaje": "Todo bien pa"}
+
+@app.get("/db-test")
+def db_test(db: Session = Depends(get_db)):
+    result = db.execute(text("SELECT DATABASE();"))
+    database_name = result.scalar()
+
+    return {
+        "mensaje": "Conexión exitosa a MySQL",
+        "base_de_datos": database_name
+    }

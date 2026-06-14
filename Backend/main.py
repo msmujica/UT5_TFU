1f415d59c468421c73d7ff3d2b15cf98be454cfc
Martin Mujica <msmujica@hotmail.com>
2026-06-14 14:52:06 -0300

    Se agregan arreglos ademas del gitignore

--- Backend/main.py
+++ Backend/main.py
@@ -1,17 +1,28 @@
 from fastapi import FastAPI
 
+from Database.connection import get_connection
+
 app = FastAPI()
+
 
 @app.get("/Health")
 def home():
     return {"mensaje": "Todo bien pa"}
 
-@app.get("/db-test")
-def db_test(db: Session = Depends(get_db)):
-    result = db.execute(text("SELECT DATABASE();"))
-    database_name = result.scalar()
 
-    return {
-        "mensaje": "Conexión exitosa a MySQL",
-        "base_de_datos": database_name
-    }
+@app.get("/db-test")
+def db_test():
+    connection = get_connection()
+
+    try:
+        with connection.cursor() as cursor:
+            cursor.execute("SELECT DATABASE() AS base_de_datos")
+            result = cursor.fetchone()
+
+        return {
+            "mensaje": "Conexión exitosa a MySQL",
+            "base_de_datos": result["base_de_datos"]
+        }
+
+    finally:
+        connection.close()
